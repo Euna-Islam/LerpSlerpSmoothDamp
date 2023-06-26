0598c1e42c1eeca7ffdc6b87211ffd5297ec4b0a
Euna Islam <euna.islam@gmail.com>
2023-06-26 18:45:38 +0300

    Added MoveTowards

---
+++ Assets/Scripts/MoveTowards.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowards : MonoBehaviour
+{
+    [SerializeField]
+    private Transform StartPoint, EndPoint;
+    private Vector3 StartPos, EndPos;
+    [SerializeField]
+    private FloatVariable Speed;
+
+    private void Start()
+    {
+        StartPos = StartPoint.position;
+        EndPos = EndPoint.position;
+        transform.position = StartPos;
+    }
+
+    private void Update()
+    {
+        MoveWithMoveTowards();
+    }
+    void MoveWithMoveTowards() {
+        var step = Speed.value * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, EndPos, step);
+
+        // Check if the position of the cube and sphere are approximately equal.
+        if (Vector3.Distance(transform.position, EndPos) < 0.001f)
+        {
+            //swap
+            Vector3 temp = StartPos;
+            StartPos = EndPos;
+            EndPos = temp;
+        }
+    }
+}
